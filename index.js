5ba4b978739d2c4bdaa95863d40aacdf89115edf
Blake Kunishige <blake3457@yahoo.com>
2018-01-28 06:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ number += 3
 number -= 2
 number *= 10
 number /= 5
+
+var number = 10
+
+function add5(){
+  number += 5 
+}
+
+function divideBy3(){
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
