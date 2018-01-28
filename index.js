a34e203a031e492d14df2477d94cbf8576ef2c39
Blake Kunishige <blake3457@yahoo.com>
2018-01-28 06:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,23 +10,17 @@ number *= 10
 number /= 5
 
 var number = 10
-
 function add5(){
   number += 5 
 }
-
 function divideBy3(){
   number /= 3
 }
-
 divideBy3()
-
 console.log(number)
 
 add5()
-
 console.log(number)
 
 divideBy3()
-
 console.log(number)
